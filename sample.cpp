3419d5df3ae6dc2eb12b62e8fbfdcab72630d90c
Catminusminus <resonafolma@gmail.com>
2019-08-20 02:15:20 +0900

    using structured bindings
    using structured bindings in samples

--- sample.cpp
+++ sample.cpp
@@ -4,14 +4,14 @@
 
 SCENN_CONSTEXPR auto mini_mnist_test() {
   using namespace scenn;
-  auto mnist_data = load_mini_mnist_data<double>();
+  auto [train_data, test_data] = load_mini_mnist_data<double>();
   auto evaluation =
       SequentialNetwork(CrossEntropy(), DenseLayer<784, 196, double>(),
                         ActivationLayer<196, double>(Sigmoid()),
                         DenseLayer<196, 3, double>(),
                         ActivationLayer<3, double>(Softmax()))
-          .train<100>(std::move(sprout::get<0>(mnist_data)), 20, 0.1)
-          .evaluate(std::move(sprout::get<1>(mnist_data)));
+          .train<100>(std::move(train_data), 20, 0.1)
+          .evaluate(std::move(test_data));
   return evaluation;
 }
 
