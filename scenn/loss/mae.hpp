49110497c16e98ab98fb1f5db5426b868cad0319
Catminusminus <resonafolma@gmail.com>
2019-09-03 01:44:08 +0900

    implemented mae
    implemented mae but not test yet

--- scenn/loss/mae.hpp
+++ scenn/loss/mae.hpp
@@ -2,9 +2,22 @@
 #define SCENN_LOSS_MSE_HPP
 
 #include <scenn/util.hpp>
-// Not implemented yet
+#include <sprout/math.hpp>
+
 namespace scenn {
 struct MAE {
+  template <class T, class U>
+  static SCENN_CONSTEXPR auto loss_function(const T& predictions,
+                                            const U& labels) {
+    auto diff = predictions - labels;
+    return std::move(diff).fmap(sprout::math::abs).sum();
+  }
+  template <class T, class U>
+  static SCENN_CONSTEXPR auto loss_derivative(const T& predictions,
+                                              const U& labels) {
+    auto diff = predictions - labels;
+    return std::move(diff).fmap([](auto&& x) { return x <= 0 ? 1 : -1; });
+  }
 };
 }  // namespace scenn
 #endif
