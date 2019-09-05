727179e8e6094358c0964f89ab8e94b64fb7bac7
Catminusminus <resonafolma@gmail.com>
2019-09-06 02:01:42 +0900

    add thresholded_relu
    implemented ThresholdedRelu to activations

---
+++ scenn/activation/thresholded_relu.hpp
@@ -0,0 +1,42 @@
+#ifndef SCENN_ACTIVATION_THRESHOLDED_RELU_HPP
+#define SCENN_ACTIVATION_THRESHOLDED_RELU_HPP
+
+#include <scenn/matrix/matrix.hpp>
+#include <scenn/util.hpp>
+
+namespace scenn {
+template <class T>
+SCENN_CONSTEXPR auto thresholded_relu(T x, T theta) {
+  if (x >= theta) return x;
+  return static_cast<T>(0);
+}
+
+template <class T>
+SCENN_CONSTEXPR auto thresholded_relu_prime(T x, T theta) {
+  if (x > theta) return static_cast<T>(1);
+  return static_cast<T>(0);
+}
+
+template <class T>
+class ThresholdedReLU {
+  T theta;
+
+ public:
+  constexpr ThresholdedReLU(T theta) : theta(theta){};
+  template <size_t M, size_t N>
+  SCENN_CONSTEXPR auto activate(const Matrix<M, N, T>& container) const {
+    return container.fmap([=](auto&& x) { return thresholded_relu<T>(x, theta); });
+  }
+  template <size_t M, size_t N>
+  SCENN_CONSTEXPR auto activate_prime(const Matrix<M, N, T>& container) const {
+    return container.fmap(
+        [=](auto&& x) { return thresholded_relu_prime<T>(x, theta); });
+  }
+  template <class V, class U>
+  SCENN_CONSTEXPR auto calc_backward_pass(V&& data, U&& delta) const {
+    return activate_prime(std::forward<V>(data)) * (std::forward<U>(delta));
+  }
+};
+}  // namespace scenn
+
+#endif
