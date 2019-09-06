d7a8bc883b795f4cc0a0104c4df4d7804b66fc2d
Catminusminus <resonafolma@gmail.com>
2019-09-07 01:58:21 +0900

    add examples dir
    start to move examples to exmaples dir from tests

---
+++ examples/mnist.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <scenn/load.hpp>
+#include <scenn/scenn.hpp>
+
+SCENN_CONSTEXPR auto mini_mnist_test() {
+  using namespace scenn;
+  auto [train_data, test_data] = load_mini_mnist_data<double>();
+  auto evaluation =
+      SequentialNetwork(CrossEntropy(), DenseLayer<784, 196, double>(),
+                        ActivationLayer<196, double>(Sigmoid()),
+                        DenseLayer<196, 3, double>(),
+                        ActivationLayer<3, double>(Softmax()))
+          .train<100>(std::move(train_data), 20, 0.1)
+          .evaluate(std::move(test_data));
+  return evaluation;
+}
+
+int main() {
+  SCENN_CONSTEXPR auto evaluation = mini_mnist_test();
+  std::cout << evaluation << std::endl;
+}
