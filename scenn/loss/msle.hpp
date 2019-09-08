3ad17b24e9e7b549e79c3575696458e91c3f8681
Catminusminus <resonafolma@gmail.com>
2019-09-08 17:07:43 +0900

    add msle to loss
    implemented mean_squared_log_error to loss

---
+++ scenn/loss/msle.hpp
@@ -0,0 +1,26 @@
+#ifndef SCENN_LOSS_MSLE_HPP
+#define SCENN_LOSS_MSLE_HPP
+
+#include <scenn/util.hpp>
+#include <sprout/math.hpp>
+
+namespace scenn {
+struct MSLE {
+  template <class T, class U>
+  static SCENN_CONSTEXPR auto loss_function(const T& predictions,
+                                            const U& labels) {
+    auto diff =
+        (predictions + 1).fmap(sprout::math::log<predictions::value_type>) -
+        (labels + 1).fmap(sprout::math::log<labels::value_type>);
+    return (diff.dot(diff.transposed()) * 0.5).to_value();
+  }
+  template <class T, class U>
+  static SCENN_CONSTEXPR auto loss_derivative(const T& predictions,
+                                              const U& labels) {
+    return ((predictions + 1).fmap(sprout::math::log<predictions::value_type>) -
+            (labels + 1).fmap(sprout::math::log<labels::value_type>)) /
+           predictions;
+  }
+};
+}  // namespace scenn
+#endif
