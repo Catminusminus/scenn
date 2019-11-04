d6ca1712fa99e3d99bba4576938b252d06f2adc5
Catminusminus <resonafolma@gmail.com>
2019-11-05 00:19:20 +0900

    implement col2im and im2col
    implement col2im and im2col to implement CNN

---
+++ scenn/experimental/util/pad.hpp
@@ -0,0 +1,19 @@
+#ifndef SCENN_EXPERIMENTAL_UTIL_PAD_HPP
+#define SCENN_EXPERIMENTAL_UTIL_PAD_HPP
+
+#include <scenn/experimental/tensor/tensor3d.hpp>
+#include <scenn/util.hpp>
+
+namespace scenn::experimental {
+template <std::size_t Pad, std::size_t L, std::size_t M, std::size_t N, class T>
+SCENN_CONSTEXPR auto pad(const Tensor3D<L, M, N, T>& tensor) {
+  Tensor3D<L, Pad + M + Pad, N, T> ret;
+  for (std::size_t i = 0; i < L; ++i)
+    for (std::size_t j = 0; j < Pad + M + Pad; ++j)
+      for (std::size_t k = 0; k < N; ++k)
+        ret(i, j, k) = Pad <= j && j < Pad + M ? tensor(i, j - Pad, k) : 0;
+  return ret;
+}
+}  // namespace scenn::experimental
+
+#endif
---
+++ tests/experimental/im2col.cpp
@@ -0,0 +1,12 @@
+#include <scenn/experimental/tensor/tensor3d.hpp>
+#include <scenn/experimental/util/im2col.hpp>
+
+int main() {
+  using namespace scenn::experimental;
+  constexpr float arr1[1][3][1] = {{{1}, {2}, {3}}};
+  constexpr auto tensor1 = make_tensor3d_from_array(arr1);
+  constexpr auto mat1 = im2col1d<2, 2, 1, 0>(tensor1);
+  constexpr float arr2[2][2] = {{1, 2}, {2, 3}};
+  constexpr auto mat2 = make_matrix_from_array(arr2);
+  static_assert(mat1 == mat2);
+}
